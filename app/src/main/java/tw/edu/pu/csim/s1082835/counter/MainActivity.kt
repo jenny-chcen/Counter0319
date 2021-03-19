7fb43284bb71d980fb313ec0dd7cd6f5b7940566
jenny <s1082835@gm.pu.edu.tw>
2021-03-19 15:50:35 +0800

    button的OnClick事件

---
+++ app/src/main/java/tw/edu/pu/csim/s1082835/counter/MainActivity.kt
@@ -0,0 +1,29 @@
+package tw.edu.pu.csim.s1082835.counter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(){
+
+    var counter: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        txv.text = counter.toString()
+
+    }
+
+    fun Add(v: View){ //View是一個視覺元件，用來判斷是哪一個button
+        if(v.id == R.id.button){ //button
+            counter++
+        }
+        else{ //button1
+            counter+=2
+        }
+        txv.text = counter.toString()
+    }
+
+}
